437144efa2cf054f8b71d852cae7da0fa3a023c8
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-06-11 01:36:47 +0530

    feat: add options to reurn sources and context in POST endpoint
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/server.py
+++ backend/src/server.py
@@ -4,39 +4,43 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 from pydantic import BaseModel
-from langserve import add_routes
 
-from src.chain import llm_chain_with_source
+from src.ensemble_chain import llm_chain_with_source
 
 class UserInput(BaseModel):
     query: str;
+    listSources: bool = False;
+    listContext: bool = False;
 
 app = FastAPI()
-
-with open("src/source_list.json") as f:
-    src_dict = json.loads(f.read())
 
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
 @app.post("/chatApp")
-async def get_response(userInput : UserInput) -> str:
-    links = []
+async def get_response(userInput : UserInput) -> dict:
+   
     user_question = userInput.query
     result = llm_chain_with_source.invoke(user_question)
 
+    links = []
+    context = []
     for i in result['context']:
-        links.append(src_dict[i.metadata['source']])
+        links.append(i.metadata['url'])
+        context.append(i.page_content)
     
     links = set(links)
 
-    response = result['answer']
-    response += "\nSources:"
-
-    for i in links:
-        response += "\n" + i
-
+    if(userInput.listSources and userInput.listContext):
+        response = {"response": result['answer'], "sources": str(links), "context": str(context)}  
+    elif(userInput.listSources):
+        response = {"response": result['answer'], "sources": str(links)}
+    elif(userInput.listContext):
+        response = {"response": result['answer'], "context": str(context)}
+    else:
+        response = {"response": result['answer']}
+    
     return response
 
 if __name__ == "__main__":
