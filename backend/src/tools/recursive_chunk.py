b38bb363ed174cc4efa53c2b0f95646a57bf04cb
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-06-11 01:35:45 +0530

    feat: use default chunking
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/tools/recursive_chunk.py
+++ backend/src/tools/recursive_chunk.py
@@ -3,33 +3,14 @@ from langchain.docstore.document import Document as LangchainDocument
 
 from transformers import AutoTokenizer
 
-from typing import Optional, List
-
-md_seperators = [
-    "\n#{1,6} ",
-    "```\n",
-    "\n\\*\\*\\*+\n",
-    "\n---+\n",
-    "\n___+\n",
-    "\n\n",
-    "\n",
-    " ",
-    "",
-]
-
-def recursive_chunking(
-    chunk_size: int,
-    knowledge_base: List[LangchainDocument],
-    tokenizer_name: Optional[str],
-) -> List[LangchainDocument]:
+def recursive_chunking(chunk_size: int, knowledge_base: list[LangchainDocument], tokenizer_name: str) -> list[LangchainDocument]:
 
     text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
         AutoTokenizer.from_pretrained(tokenizer_name),
         chunk_size=chunk_size,
         chunk_overlap=int(chunk_size / 10),
         add_start_index=True,
-        strip_whitespace=True,
-        separators=md_seperators,
+        strip_whitespace=True
     )
 
     docs_processed = []
