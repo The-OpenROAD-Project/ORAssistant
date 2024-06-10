9ce7a3a74def299bca2370c5ac35e7bc64170cec
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-06-11 01:35:25 +0530

    feat: use native md splitter, add urls as metadata
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/tools/process_md.py
+++ backend/src/tools/process_md.py
@@ -1,45 +1,37 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import json
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter,Language
 from langchain.docstore.document import Document as LangchainDocument
 from langchain_community.document_loaders import DirectoryLoader
 
 from src.tools.recursive_chunk import recursive_chunking
 
-md_seperators = [
-    "\n#{1,6} ",
-    "```\n",
-    "\n\\*\\*\\*+\n",
-    "\n---+\n",
-    "\n___+\n",
-    "\n\n",
-    "\n",
-    " ",
-    "",
-    "```"
-]
-
 def chunk_markdown(embeddings_model_name: str , files_path: str, chunk_size: int) -> list[LangchainDocument]:
-    loader = DirectoryLoader(files_path, glob="**/*.md" ,show_progress=True)
-    documents = loader.load()
+    with open("src/source_list.json") as f:
+        src_dict = json.loads(f.read())
 
-    text_splitter = RecursiveCharacterTextSplitter(
+    markdown_splitter = RecursiveCharacterTextSplitter.from_language(
         chunk_size=chunk_size, 
         chunk_overlap=int(chunk_size/10), 
         add_start_index=True, 
         strip_whitespace=True,  
-        separators=md_seperators,
+        language=Language.PYTHON
     )
+
+    loader = DirectoryLoader(files_path, glob="**/*.md" ,show_progress=True)
+    documents = loader.load()
    
-    documents_knowledge_base = [
-        LangchainDocument(page_content=doc.page_content, metadata=doc.metadata)
-        for doc in (documents)
-    ]
+    documents_knowledge_base = []
+    for doc in documents:
+        new_metadata = {"url":src_dict[doc.metadata['source']],"source":doc.metadata['source']}
+        documents_knowledge_base.append(LangchainDocument(page_content=doc.page_content, metadata=new_metadata))
 
     docs_split = []
     for doc in documents_knowledge_base:
-        docs_split.extend(text_splitter.split_documents([doc]))
+        docs_split.extend(markdown_splitter.split_documents([doc]))
 
     docs_chunked = recursive_chunking(
-        256,
+        int(chunk_size/2),
         docs_split,
         tokenizer_name=embeddings_model_name,
     )
