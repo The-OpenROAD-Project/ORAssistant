aae022827a8ce04d2c646bd3c49e994a5e2d8cb8
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-06-15 17:42:11 +0530

    style: format code with ruff
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,47 +1,53 @@
-import json
-
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 from pydantic import BaseModel
 
-from src.ensemble_chain import llm_chain_with_source
+from .ensemble_chain import llm_chain_with_source
+
 
 class UserInput(BaseModel):
-    query: str;
-    listSources: bool = False;
-    listContext: bool = False;
+    query: str
+    listSources: bool = False
+    listContext: bool = False
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
+
 @app.post("/chatApp")
-async def get_response(userInput : UserInput) -> dict:
-   
+async def get_response(userInput: UserInput) -> dict:
     user_question = userInput.query
     result = llm_chain_with_source.invoke(user_question)
 
     links = []
     context = []
-    for i in result['context']:
-        links.append(i.metadata['url'])
+    for i in result["context"]:
+        links.append(i.metadata["url"])
         context.append(i.page_content)
-    
+
     links = set(links)
 
-    if(userInput.listSources and userInput.listContext):
-        response = {"response": result['answer'], "sources": str(links), "context": str(context)}  
-    elif(userInput.listSources):
-        response = {"response": result['answer'], "sources": str(links)}
-    elif(userInput.listContext):
-        response = {"response": result['answer'], "context": str(context)}
+    if userInput.listSources and userInput.listContext:
+        response = {
+            "response": result["answer"],
+            "sources": str(links),
+            "context": str(context),
+        }
+    elif userInput.listSources:
+        response = {"response": result["answer"], "sources": str(links)}
+    elif userInput.listContext:
+        response = {"response": result["answer"], "context": str(context)}
     else:
-        response = {"response": result['answer']}
-    
+        response = {"response": result["answer"]}
+
     return response
+
 
 if __name__ == "__main__":
     import uvicorn
