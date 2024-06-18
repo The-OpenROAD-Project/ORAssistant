1d9933e464397093ab444ddf7cdc0e1cb0a1e862
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-06-19 01:23:28 +0530

    feat: add a faiss_db object to class and move file to vectorstores
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/vectorstores/faiss.py
+++ backend/src/vectorstores/faiss.py
@@ -1,71 +1,93 @@
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.utils import DistanceStrategy
+from langchain.docstore.document import Document as LangchainDocument
 
-from .tools.process_md import chunk_markdown
-from .tools.process_json import generate_knowledge_base
+from ..tools.process_md import chunk_md_docs,chunk_md_manpages
+from ..tools.process_json import generate_knowledge_base
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_vertexai import VertexAIEmbeddings
-
-from typing import Union
-
 
 class FAISSVectorDatabase:
     def __init__(
         self,
         embeddings_model_name: str,
+        distance_strategy: DistanceStrategy = DistanceStrategy.COSINE,
         print_progress: bool = False,
         debug: bool = False,
         use_cuda: bool = False,
     ):
         self.embeddings_model_name = embeddings_model_name
-        if use_cuda:
-            self.embedding_model = HuggingFaceEmbeddings(
-                model_name=self.embeddings_model_name,
-                multi_process=False,
-                encode_kwargs={"normalize_embeddings": True},
-                model_kwargs={"device": "cuda"},
-            )
-        else:
-            self.embedding_model = HuggingFaceEmbeddings(
-                model_name=self.embeddings_model_name,
-                multi_process=False,
-                encode_kwargs={"normalize_embeddings": True},
-            )
+
+        model_kwargs = {"device": "cuda"} if use_cuda else {}
+        self.embedding_model = HuggingFaceEmbeddings(
+            model_name=self.embeddings_model_name,
+            multi_process=False,
+            encode_kwargs={"normalize_embeddings": True},
+            model_kwargs=model_kwargs,
+        )
 
         # self.embedding_model = GoogleGenerativeAIEmbeddings.model()
         # self.embedding_model = VertexAIEmbeddings(model_name="textembedding-gecko@001")
+
         self.print_progress = print_progress
         self.debug = debug
+        self.distance_strategy = distance_strategy
+        
+        self._faiss_db = FAISS.from_documents(
+            documents=[LangchainDocument(page_content="")],
+            embedding=self.embedding_model,
+            distance_strategy=self.distance_strategy,
+        )
+    
+    @property
+    def get_db(self) -> FAISS:
+        return self._faiss_db
 
-    def process_md(
+    def process_md_docs(
         self, folder_paths: list[str], chunk_size: int = 1000, return_docs: bool = False
-    ) -> Union[FAISS, tuple[FAISS, list]]:
+    ) -> list:
         if self.print_progress is True:
-            print("Processing markdown files...")
+            print("Processing markdown docs...")
 
         docs_processed = []
 
         for file_path in folder_paths:
             if self.print_progress is True:
                 print(f"Processing [{file_path}]...")
-                docs_processed.extend(chunk_markdown(
+                docs_processed.extend(chunk_md_docs(
                     embeddings_model_name=self.embeddings_model_name,
                     files_path=file_path,
                     chunk_size=chunk_size,
                 ))
 
-        md_vector_db = FAISS.from_documents(
-            documents=docs_processed,
-            embedding=self.embedding_model,
-            distance_strategy=DistanceStrategy.COSINE,
-        )
-
+        self._faiss_db.add_documents(docs_processed)
+        
         if return_docs:
-            return md_vector_db, docs_processed
-        else:
-            return md_vector_db
+            return docs_processed
+        
+    def process_md_manpages(
+        self, folder_paths: list[str], chunk_size: int = 1000, return_docs: bool = False
+    ) -> list:
+        if self.print_progress is True:
+            print("Processing markdown manpages...")
+
+        docs_processed = []
+
+        for file_path in folder_paths:
+            if self.print_progress is True:
+                print(f"Processing [{file_path}]...")
+                docs_processed.extend(chunk_md_manpages(
+                    embeddings_model_name=self.embeddings_model_name,
+                    files_path=file_path,
+                    chunk_size=chunk_size,
+                ))
+
+        self._faiss_db.add_documents(docs_processed)
+        
+        if return_docs:
+            return docs_processed
 
     def process_json(self, folder_paths: list[str]) -> FAISS:
         if self.print_progress is True:
