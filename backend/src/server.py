a2cbf7e83e1bdc5baeab5ffe184b7d72a1ee36e3
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-06-20 01:03:56 +0530

    feat: use hybrid retriever
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/server.py
+++ backend/src/server.py
@@ -3,7 +3,9 @@ from fastapi.responses import RedirectResponse
 
 from pydantic import BaseModel
 
-from .chains.ensemble_chain import llm_chain_with_source
+from .chains.hybrid_retriever_chain import HybridRetrieverChain
+
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 class UserInput(BaseModel):
@@ -14,6 +16,34 @@ class UserInput(BaseModel):
 
 app = FastAPI()
 
+llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=1)
+
+prompt_template_str = """
+    Use the following context:
+
+    {context}
+
+    -------------------------------------------------------------------------------------------------
+    Your task is to act as a knowledgeable assistant for users seeking information and guidance about the OpenROAD project. Avoid speculating or inventing information beyond the scope of the provided data.
+    Note that OR refers to OpenROAD and ORFS refers to OpenROAD-Flow-Scripts
+
+    Give a detailed answer to this question: 
+    {question}
+
+    """
+
+retriever = HybridRetrieverChain(
+    llm_model=llm,
+    prompt_template_str=prompt_template_str,
+    embeddings_model_name="BAAI/bge-large-en-v1.5",
+    contextual_rerank=True,
+    use_cuda=True,
+    docs_path=["./data/markdown/ORFS_docs", "./data/markdown/OR_docs"],
+    manpages_path=["./data/markdown/manpages"],
+)
+
+retriever_chain = retriever.get_llm_chain()
+
 
 @app.get("/")
 async def redirect_root_to_docs():
@@ -23,7 +53,7 @@ async def redirect_root_to_docs():
 @app.post("/chatApp")
 async def get_response(userInput: UserInput) -> dict:
     user_question = userInput.query
-    result = llm_chain_with_source.invoke(user_question)
+    result = retriever_chain.invoke(user_question)
 
     links = []
     context = []
