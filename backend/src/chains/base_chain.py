fa4e7c309e61aca7beffd022945bedbbf34ddd84
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-06-19 14:55:40 +0530

    feat: add base chain class
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

---
+++ backend/src/chains/base_chain.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+
+from langchain.prompts import ChatPromptTemplate
+
+from langchain_core.output_parsers import StrOutputParser
+
+# from langchain_google_vertexai import ChatVertexAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+class BaseChain:
+    def __init__(self, llm_model, prompt_template_str):
+        self.llm_model = llm_model
+        self.prompt_template = ChatPromptTemplate.from_template(prompt_template_str)
+
+        self.llm_chain = None
+
+    def create_llm_chain(self):
+        self.llm_chain = self.prompt_template | self.llm_model | StrOutputParser()
+
+    def get_llm_chain(self):
+        if self.llm_chain is None:
+            self.create_llm_chain()
+        return self.llm_chain
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=1)
+    prompt_template_str = """
+    Give a detailed answer to this question: 
+    {question}
+    """
+
+    basechain = BaseChain(llm_model=llm, prompt_template_str=prompt_template_str)
+
+    basechain = basechain.get_llm_chain()
+
+    while True:
+        user_question = input("\n\nAsk a question: ")
+        result = basechain.invoke(user_question)
+        print(result)
