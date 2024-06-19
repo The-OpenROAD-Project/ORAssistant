92909ce0aa79600a3eba078e4ab0c2733b9fc1d9
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-06-19 23:26:48 +0530

    chore: update import
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/vectorstores/faiss.py
+++ backend/src/vectorstores/faiss.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores.utils import DistanceStrategy
 from langchain.docstore.document import Document as LangchainDocument
 
