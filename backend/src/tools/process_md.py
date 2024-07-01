7a289df42f06a0d8da7bac45be2e52941adbf9de
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-07-02 03:10:08 +0530

    feat: refactor markdown loading and processing
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/tools/process_md.py
+++ backend/src/tools/process_md.py
@@ -3,14 +3,32 @@ import os
 import glob
 from tqdm import tqdm
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+from bs4 import BeautifulSoup
+import markdown as md
+
 from langchain.docstore.document import Document as LangchainDocument
-from langchain_community.document_loaders import DirectoryLoader
 
-from .recursive_chunk import recursive_chunking
+from .recursive_chunk import chunk_documents
 
 
-def chunk_md_docs(
+def md_to_text(md_content):
+    html = md.markdown(md_content)
+    soup = BeautifulSoup(html, features="html.parser")
+    return soup.get_text()
+
+
+def load_docs(files_path: str) -> list[LangchainDocument]:
+    md_files = glob.glob(os.path.join(files_path, "**/*.md"), recursive=True)
+    documents = []
+    for file_path in tqdm(md_files, desc="Loading Markdown files"):
+        with open(file_path, "r", encoding="utf-8") as file:
+            content = md_to_text(file.read())
+            metadata = {"source": file_path[2:]}
+            documents.append(LangchainDocument(page_content=content, metadata=metadata))
+    return documents
+
+
+def process_md_docs(
     embeddings_model_name: str, files_path: str, chunk_size: int
 ) -> list[LangchainDocument]:
     """
@@ -24,21 +42,12 @@ def chunk_md_docs(
     with open("src/source_list.json") as f:
         src_dict = json.loads(f.read())
 
-    md_files = glob.glob(os.path.join(files_path, "**/*.md"), recursive=True)
-    documents = []
-    for file_path in tqdm(md_files, desc="Loading Markdown files"):
-        with open(file_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-            metadata = {"source": file_path[2:]}
-            documents.append(LangchainDocument(page_content=content, metadata=metadata))
+    documents = load_docs(files_path=files_path)
 
-    markdown_splitter = RecursiveCharacterTextSplitter.from_language(
-        chunk_size=chunk_size,
-        chunk_overlap=int(chunk_size / 10),
-        add_start_index=True,
-        strip_whitespace=True,
-        language=Language.MARKDOWN,
-    )
+    with open("documents.txt", "w") as f:
+        for i, doc in enumerate(documents):
+            f.write(f"Doc ID: {i + 1}\n")
+            f.write(str(doc.page_content) + "\n")
 
     documents_knowledge_base = []
     for doc in documents:
@@ -50,20 +59,16 @@ def chunk_md_docs(
             LangchainDocument(page_content=doc.page_content, metadata=new_metadata)
         )
 
-    docs_split = []
-    for doc in documents_knowledge_base:
-        docs_split.extend(markdown_splitter.split_documents([doc]))
-
-    docs_chunked = recursive_chunking(
-        int(chunk_size / 2),
-        docs_split,
+    docs_chunked = chunk_documents(
+        chunk_size,
+        documents_knowledge_base,
         tokenizer_name=embeddings_model_name,
     )
 
     return docs_chunked
 
 
-def chunk_md_manpages(files_path: str) -> list[LangchainDocument]:
+def process_md_manpages(files_path: str) -> list[LangchainDocument]:
     """
     For processing manpages
     """
@@ -72,7 +77,6 @@ def chunk_md_manpages(files_path: str) -> list[LangchainDocument]:
         print(f"{files_path} is not populated, returning empty list.")
         return []
 
-    loader = DirectoryLoader(files_path, glob="**/*.md", show_progress=True)
-    documents_knowledge_base = loader.load()
+    documents = load_docs(files_path=files_path)
 
-    return documents_knowledge_base
+    return documents
