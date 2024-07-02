c87d8b5f08a51f52bd4822026ccce1680304fdf4
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-07-02 09:46:54 +0530

    feat: Add hybrid, similarity, and multi retriever chains
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/api/routers/chains.py
+++ backend/src/api/routers/chains.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 from ...chains.hybrid_retriever_chain import HybridRetrieverChain
+from ...chains.similarity_retriever_chain import SimilarityRetrieverChain
+from ...chains.multi_retriever_chain import MultiRetrieverChain
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -47,13 +49,98 @@ hybrid_retriever = HybridRetrieverChain(
     manpages_path=["./data/markdown/manpages"],
 )
 hybrid_retriever.create_hybrid_retriever()
-hybrid_retriever_chain = hybrid_retriever.get_llm_chain()
+hybrid_llm_chain = hybrid_retriever.get_llm_chain()
+
+sim_retriever_chain = SimilarityRetrieverChain(
+    llm_model=llm,
+    prompt_template_str=prompt_template_str,
+    embeddings_model_name="BAAI/bge-large-en-v1.5",
+    use_cuda=True,
+    docs_path=["./data/markdown/ORFS_docs", "./data/markdown/OR_docs"],
+    manpages_path=["./data/markdown/manpages"],
+)
+sim_retriever_chain.create_similarity_retriever()
+sim_llm_chain = sim_retriever_chain.get_llm_chain()
+
+multi_retriever_chain = MultiRetrieverChain(
+    llm_model=llm,
+    prompt_template_str=prompt_template_str,
+    embeddings_model_name="BAAI/bge-large-en-v1.5",
+    use_cuda=True,
+    docs_path=["./data/markdown/ORFS_docs", "./data/markdown/OR_docs"],
+    manpages_path=["./data/markdown/manpages"],
+)
+multi_retriever_chain.create_multi_retriever()
+multi_llm_chain = multi_retriever_chain.get_llm_chain()
+
 
 
 @router.post("/hybrid")
+async def get_hybrid_response(user_input: UserInput) -> dict:
+    user_question = user_input.query
+    result = hybrid_llm_chain.invoke(user_question)
+
+    links = []
+    context = []
+    for i in result["context"]:
+        if "url" in i.metadata:
+            links.append(i.metadata["url"])
+        elif "source" in i.metadata:
+            links.append(i.metadata["source"])
+        context.append(i.page_content)
+
+    links_set = set(links)
+
+    if user_input.list_sources and user_input.list_context:
+        response = {
+            "response": result["answer"],
+            "sources": (links_set),
+            "context": (context),
+        }
+    elif user_input.list_sources:
+        response = {"response": result["answer"], "sources": (links_set)}
+    elif user_input.list_context:
+        response = {"response": result["answer"], "context": (context)}
+    else:
+        response = {"response": result["answer"]}
+
+    return response
+
+@router.post("/sim")
+async def get_sim_response(user_input: UserInput) -> dict:
+    user_question = user_input.query
+    result = hybrid_llm_chain.invoke(user_question)
+
+    links = []
+    context = []
+    for i in result["context"]:
+        if "url" in i.metadata:
+            links.append(i.metadata["url"])
+        elif "source" in i.metadata:
+            links.append(i.metadata["source"])
+        context.append(i.page_content)
+
+    links_set = set(links)
+
+    if user_input.list_sources and user_input.list_context:
+        response = {
+            "response": result["answer"],
+            "sources": (links_set),
+            "context": (context),
+        }
+    elif user_input.list_sources:
+        response = {"response": result["answer"], "sources": (links_set)}
+    elif user_input.list_context:
+        response = {"response": result["answer"], "context": (context)}
+    else:
+        response = {"response": result["answer"]}
+
+    return response
+
+@router.post("/ensemble")
 async def get_response(user_input: UserInput) -> dict:
     user_question = user_input.query
-    result = hybrid_retriever_chain.invoke(user_question)
+    result = hybrid_llm_chain.invoke(user_question)
 
     links = []
     context = []
