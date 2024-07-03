080470150744c6aa9f2707c573a7590e058e4f15
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-07-04 01:10:58 +0530

    feat: load prompt templates from a sep folder
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/api/routers/chains.py
+++ backend/src/api/routers/chains.py
@@ -1,9 +1,13 @@
+import os
+
 from fastapi import APIRouter
 from pydantic import BaseModel
 
 from ...chains.hybrid_retriever_chain import HybridRetrieverChain
 from ...chains.similarity_retriever_chain import SimilarityRetrieverChain
 from ...chains.multi_retriever_chain import MultiRetrieverChain
+
+from ...prompts.answer_prompts import summarise_prompt_template
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 
@@ -17,32 +21,21 @@ class UserInput(BaseModel):
 
 
 load_dotenv()
+use_cuda = os.getenv("USE_CUDA")
 
 llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=1)
+hf_embdeddings = "BAAI/bge-large-en-v1.5"
+hf_reranker = "BAAI/bge-reranker-base"
 
 router = APIRouter(prefix="/chains", tags=["chains"])
 
-prompt_template_str = """
-    Use the following context:
-
-    {context}
-
-    -------------------------------------------------------------------------------------------------
-    Your task is to act as a knowledgeable assistant for users seeking information and guidance about the OpenROAD project. Avoid speculating or inventing information beyond the scope of the provided data.
-    Note that OR refers to OpenROAD and ORFS refers to OpenROAD-Flow-Scripts
-
-    Give a detailed answer to this question: 
-    {question}
-
-    """
-
 hybrid_retriever = HybridRetrieverChain(
     llm_model=llm,
-    prompt_template_str=prompt_template_str,
-    embeddings_model_name="BAAI/bge-large-en-v1.5",
-    reranking_model_name="BAAI/bge-reranker-base",
+    prompt_template_str=summarise_prompt_template,
+    embeddings_model_name=hf_embdeddings,
+    reranking_model_name=hf_reranker,
     contextual_rerank=True,
-    use_cuda=True,
+    use_cuda=use_cuda,
     docs_path=["./data/markdown/ORFS_docs", "./data/markdown/OR_docs"],
     manpages_path=["./data/markdown/manpages"],
 )
@@ -51,9 +44,9 @@ hybrid_llm_chain = hybrid_retriever.get_llm_chain()
 
 sim_retriever_chain = SimilarityRetrieverChain(
     llm_model=llm,
-    prompt_template_str=prompt_template_str,
-    embeddings_model_name="BAAI/bge-large-en-v1.5",
-    use_cuda=True,
+    prompt_template_str=summarise_prompt_template,
+    embeddings_model_name=hf_embdeddings,
+    use_cuda=use_cuda,
     docs_path=["./data/markdown/ORFS_docs", "./data/markdown/OR_docs"],
     manpages_path=["./data/markdown/manpages"],
 )
@@ -62,18 +55,20 @@ sim_llm_chain = sim_retriever_chain.get_llm_chain()
 
 multi_retriever_chain = MultiRetrieverChain(
     llm_model=llm,
-    prompt_template_str=prompt_template_str,
-    embeddings_model_name="BAAI/bge-large-en-v1.5",
-    use_cuda=True,
+    prompt_template_str=summarise_prompt_template,
+    embeddings_model_name=hf_embdeddings,
+    use_cuda=use_cuda,
     docs_path=["./data/markdown/ORFS_docs", "./data/markdown/OR_docs"],
     manpages_path=["./data/markdown/manpages"],
 )
 multi_retriever_chain.create_multi_retriever()
 multi_llm_chain = multi_retriever_chain.get_llm_chain()
 
+
 @router.get("/listAll")
 async def list_all_chains() -> list:
-    return ["/chains/hybrid","/chains/sim","/chains/ensemble"]
+    return ["/chains/hybrid", "/chains/sim", "/chains/ensemble"]
+
 
 @router.post("/hybrid")
 async def get_hybrid_response(user_input: UserInput) -> dict:
@@ -106,6 +101,7 @@ async def get_hybrid_response(user_input: UserInput) -> dict:
 
     return response
 
+
 @router.post("/sim")
 async def get_sim_response(user_input: UserInput) -> dict:
     user_question = user_input.query
@@ -136,6 +132,7 @@ async def get_sim_response(user_input: UserInput) -> dict:
         response = {"response": result["answer"]}
 
     return response
+
 
 @router.post("/ensemble")
 async def get_response(user_input: UserInput) -> dict:
