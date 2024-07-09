0b8150d613677c0dbcd3be632545ada9f85d05c8
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-07-09 12:56:45 +0530

    feat: update imports in base_chain.py
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/chains/base_chain.py
+++ backend/src/chains/base_chain.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
 
 from langchain_core.output_parsers import StrOutputParser
-
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_vertexai import ChatVertexAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 from typing import Optional, Union, Any
 
 
