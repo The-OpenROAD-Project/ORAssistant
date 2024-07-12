715dfcee63245f7ae0ff81c9fe541f5b53ee9130
Song Luar <espsluar@gmail.com>
2024-07-12 08:16:20 +0800

    Feat: add pre-commits (#35)
    * initial commit
    
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>
    
    * chore: Update CI configuration and dependencies
    
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>
    
    * pass all ci
    
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,8 @@ import uvicorn
 
 
 def main() -> None:
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
