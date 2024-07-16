f998e068cdb48c207bba8e4d581ad8953bac27d0
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-07-17 04:44:23 +0530

    refactor: Improve formatting of document titles in format_docs.py
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/tools/format_docs.py
+++ backend/src/tools/format_docs.py
@@ -4,21 +4,28 @@ from langchain.docstore.document import Document
 def format_docs(docs: list[Document]) -> str:
     formatted_text = ''
     for doc in docs:
-        if doc.metadata is not None and 'source' in doc.metadata:
-            title = str(doc.metadata['source'].split('data/markdown')[1])
-            title = (
-                title.replace('.md', '')
-                .replace('/', ' - ')
-                .replace('ORFS', 'OpenROAD-flow-scripts')
-                .replace('OR', 'OpenROAD')
-                .replace('_', ' ')
-                .upper()
-            )
-            formatted_text += (
-                f'\n{title}\n{doc.page_content}'
-                + '\n- - - - - -  - - - - - - - - - - - - - - - -\n\n'
-            )
+        source = doc.metadata.get('source', '')
+        if 'data/markdown' in source:
+            parts = source.split('data/markdown')
+            if len(parts) > 1:
+                title = parts[1]
+                title = (
+                    title.replace('.md', '')
+                    .replace('/', ' - ')
+                    .replace('ORFS', 'OpenROAD-flow-scripts')
+                    .replace('OR', 'OpenROAD')
+                    .replace('_', ' ')
+                    .upper()
+                )
+                formatted_text += (
+                    f'\n{title}\n{doc.page_content}'
+                    + '\n- - - - - -  - - - - - - - - - - - - - - - -\n\n'
+                )
+            else:
+                print('Title part not found. Skipping')
+                formatted_text += doc.page_content
         else:
+            print('Source not found. Skipping')
             formatted_text += doc.page_content
 
     return formatted_text
