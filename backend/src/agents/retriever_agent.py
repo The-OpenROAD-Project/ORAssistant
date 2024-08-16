b870a58e3c198bc75450e8277a3a079b2ef8cb13
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-08-16 08:56:47 +0530

    feat: update tool prompts
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/agents/retriever_agent.py
+++ backend/src/agents/retriever_agent.py
@@ -1,6 +1,7 @@
 from ..chains.hybrid_retriever_chain import HybridRetrieverChain
 
 from langchain_core.tools import tool
+
 from langchain.retrievers import EnsembleRetriever
 from langchain.retrievers import ContextualCompressionRetriever
 
@@ -38,6 +39,8 @@ class RetrieverAgent:
     yosys_rtdocs_retriever: Optional[
         Union[EnsembleRetriever, ContextualCompressionRetriever]
     ]
+    tool_descriptions: str = ""
+
 
     def initialize(
         self,
@@ -45,19 +48,6 @@ class RetrieverAgent:
         reranking_model_name: str,
         use_cuda: bool = False,
     ) -> None:
-        yosys_rtdocs_retriever_chain = HybridRetrieverChain(
-            embeddings_config=embeddings_config,
-            reranking_model_name=reranking_model_name,
-            use_cuda=use_cuda,
-            html_docs_path=['./data/rtdocs/yosyshq.readthedocs.io/'],
-            weights=[0.6, 0.2, 0.2],
-            contextual_rerank=True,
-            search_k=search_k,
-            chunk_size=chunk_size,
-        )
-        yosys_rtdocs_retriever_chain.create_hybrid_retriever()
-        RetrieverAgent.yosys_rtdocs_retriever = yosys_rtdocs_retriever_chain.retriever
-
         install_retriever_chain = HybridRetrieverChain(
             embeddings_config=embeddings_config,
             reranking_model_name=reranking_model_name,
@@ -80,8 +70,8 @@ class RetrieverAgent:
             embeddings_config=embeddings_config,
             reranking_model_name=reranking_model_name,
             use_cuda=use_cuda,
-            markdown_docs_path=['./data/markdown/OR_docs/tools'],
-            manpages_path=[
+            markdown_docs_path=[
+                './data/markdown/OR_docs/tools'
                 './data/markdown/manpages/man1',
                 './data/markdown/manpages/man2',
                 './data/markdown/gh_discussions/Query',
@@ -95,6 +85,38 @@ class RetrieverAgent:
         )
         commands_retriever_chain.create_hybrid_retriever()
         RetrieverAgent.commands_retriever = commands_retriever_chain.retriever
+ 
+        general_retriever_chain = HybridRetrieverChain(
+            embeddings_config=embeddings_config,
+            reranking_model_name=reranking_model_name,
+            use_cuda=use_cuda,
+            markdown_docs_path=[
+                './data/markdown/OR_docs',
+                './data/markdown/ORFS_docs',
+                './data/markdown/gh_discussions',
+                './data/markdown/manpages/man1',
+                './data/markdown/manpages/man2',
+            ],
+            weights=[0.6, 0.2, 0.2],
+            contextual_rerank=True,
+            search_k=search_k,
+            chunk_size=chunk_size,
+        )
+        general_retriever_chain.create_hybrid_retriever()
+        RetrieverAgent.general_retriever = general_retriever_chain.retriever
+
+        yosys_rtdocs_retriever_chain = HybridRetrieverChain(
+            embeddings_config=embeddings_config,
+            reranking_model_name=reranking_model_name,
+            use_cuda=use_cuda,
+            html_docs_path=['./data/rtdocs/yosyshq.readthedocs.io/'],
+            weights=[0.6, 0.2, 0.2],
+            contextual_rerank=True,
+            search_k=search_k,
+            chunk_size=chunk_size,
+        )
+        yosys_rtdocs_retriever_chain.create_hybrid_retriever()
+        RetrieverAgent.yosys_rtdocs_retriever = yosys_rtdocs_retriever_chain.retriever
 
         opensta_retriever_chain = HybridRetrieverChain(
             embeddings_config=embeddings_config,
@@ -108,27 +130,6 @@ class RetrieverAgent:
         )
         opensta_retriever_chain.create_hybrid_retriever()
         RetrieverAgent.opensta_retriever = opensta_retriever_chain.retriever
-
-        general_retriever_chain = HybridRetrieverChain(
-            embeddings_config=embeddings_config,
-            reranking_model_name=reranking_model_name,
-            use_cuda=use_cuda,
-            markdown_docs_path=[
-                './data/markdown/ORFS_docs',
-                './data/markdown/OR_docs',
-                './data/markdown/gh_discussions',
-            ],
-            manpages_path=[
-                './data/markdown/manpages/man1',
-                './data/markdown/manpages/man2',
-            ],
-            weights=[0.6, 0.2, 0.2],
-            contextual_rerank=True,
-            search_k=search_k,
-            chunk_size=chunk_size,
-        )
-        general_retriever_chain.create_hybrid_retriever()
-        RetrieverAgent.general_retriever = general_retriever_chain.retriever
 
         errinfo_retriever_chain = HybridRetrieverChain(
             embeddings_config=embeddings_config,
@@ -150,10 +151,10 @@ class RetrieverAgent:
     @tool
     def retrieve_general(query: str) -> Tuple[str, list[str], list[str]]:
         """
-        Retrieve comprehensive and detailed information pertaining to the OpenROAD project and OpenROAD-Flow-Scripts. \
-        This includes, but is not limited to, general information, specific functionalities, usage guidelines, \
-        troubleshooting steps, and best practices. The tool is designed to assist users by providing clear, accurate, \
-        and relevant information that enhances their understanding and efficient use of OpenROAD and OpenROAD-Flow-Scripts.
+        Retrieve comprehensive and detailed information pertaining to the OpenROAD project and OpenROAD-Flow-Scripts.\
+        This includes, but is not limited to, general information, specific functionalities, usage guidelines,\
+        troubleshooting steps, and best practices. The tool is designed to assist users by providing clear, accurate,\
+        and relevant information that enhances their understanding and efficient use of OpenROAD and OpenROAD-Flow-Scripts.\
         """
         if RetrieverAgent.general_retriever is None:
             raise ValueError('General Retriever not initialized')
@@ -165,41 +166,38 @@ class RetrieverAgent:
     @tool
     def retrieve_cmds(query: str) -> Tuple[str, list[str], list[str]]:
         """
-        Retrieve information on the commands and tools available in the OpenROAD project and OpenROAD-Flow-Scripts. \
-        This includes descriptions, usage guidelines, command syntax, examples, and best practices.
-        The tool assists users by delivering clear, accurate, and relevant details to help them effectively utilize the following commands and tools within OpenROAD and OpenROAD-Flow-Scripts:
- 
-        Here's a list of all the tools available:
-        ANT: Antenna Rule Checker
-        CTS: Clock Tree Synthesis
-        DFT: Design For Testing
-        DPL: Detailed Placement
-        DRT: Detailed Routing
-        FIN: Metal Fill
-        GPL: Global Floorplanning
-        GPL: Global Placement
-        GRT: Global Routing
-        GUI: Graphical User Interface
-        IFP: Initialize Floorplan
-        MPL: Macro Placement
-        MPL: Hierarchical Macro Placement
-        ODB: OpenDB
-        Chip-level Connections
-        PAD: Pad
-        PAR: Partition Manager
-        PDN: Power Distribution Network
-        PPL: Pin Placement
-        PSM: IR Drop Analysis
-        RSX: Parasitics Extraction
-        RMP: Restructure
-        RSZ: Gate Resizer
-        STT: Rectilinear Steiner Tree
-        TAP: TapCell
-        UPF: Read Unified Power Format
-        UTL: Utilities
-
-        The tools can be represented by their abbreviations as well: ANT,CTS,DFT,DPL,DRT,FIN,GPL,GRT,GUI,IFP,MPL,ODB,PAD,PAR,PDN,PPL,PSM,RSX,RMP,RSZ,STT,TAP,UPF,UTL     
-        
+        Retrieve information on the commands available in the OpenROAD project and OpenROAD-Flow-Scripts.\
+        This includes descriptions, usage guidelines, command syntax, examples, and best practices.\
+        This tool assists users by delivering clear, accurate, and\
+        relevant details to help them effectively utilize the following commands within OpenROAD and OpenROAD-Flow-Scripts:\
+        Here's a list of all the commands available:
+        ANT: Antenna Rule Checker\
+        CTS: Clock Tree Synthesis\
+        DFT: Design For Testing\
+        DPL: Detailed Placement\
+        DRT: Detailed Routing\
+        FIN: Metal Fill\
+        GPL: Global Floorplanning\
+        GPL: Global Placement\
+        GRT: Global Routing\
+        GUI: Graphical User Interface\
+        IFP: Initialize Floorplan\
+        MPL: Macro Placement\
+        MPL: Hierarchical Macro Placement\
+        ODB: OpenDB\
+        Chip-level Connections\
+        PAD: Pad\
+        PAR: Partition Manager\
+        PDN: Power Distribution Network\
+        PPL: Pin Placement\
+        PSM: IR Drop Analysis\
+        RSX: Parasitics Extraction\
+        RMP: Restructure\
+        RSZ: Gate Resizer\
+        STT: Rectilinear Steiner Tree\
+        TAP: TapCell\
+        UPF: Read Unified Power Format\
+        UTL: Utilities\
         """
         if RetrieverAgent.commands_retriever is None:
             raise ValueError('Commands Retriever not initialized')
@@ -211,13 +209,11 @@ class RetrieverAgent:
     @tool
     def retrieve_install(query: str) -> Tuple[str, list[str], list[str]]:
         """
-        Retrieve comprehensive and detailed information pertaining to the installaion of OpenROAD project and OpenROAD-Flow-Scripts. \
-        This includes, but is not limited to, various dependencies, system requirements, installation methods such as,
-        - Building from source
-        - Using Docker
-        - Using pre-built binaries
-    
-        This tool is designed to assist users by providing clear, accurate, and relevant information that enhances their understanding and efficient use of OpenROAD and OpenROAD-Flow-Scripts.
+        Retrieve comprehensive and detailed information pertaining to the installaion of OpenROAD project and OpenROAD-Flow-Scripts.\
+        This includes, but is not limited to, various dependencies, system requirements, installation methods such as,\
+        - Building from source\
+        - Using Docker\
+        - Using pre-built binaries\
         """
         if RetrieverAgent.install_retriever is None:
             raise ValueError('Install Retriever not initialized')
@@ -229,27 +225,23 @@ class RetrieverAgent:
     @tool
     def retrieve_opensta(query: str) -> Tuple[str, list[str], list[str]]:
         """
-        Retrieve detailed and comprehensive information about OpenSTA and its various commands.
-        
+        Retrieve detailed and comprehensive information about OpenSTA and its various commands.\
         OpenSTA is an open-source gate-level static timing verifier that has been used by many design houses.\
-        As a stand-alone executable it can be used to verify the timing of a design using standard file formats.
-
-        Verilog netlist
-        Liberty library
-        SDC timing constraints
-        SDF delay annotation
-        SPEF parasitics
-
-        Command Line Arguments: Detailed information about the different command line arguments that can be used with OpenSTA.
-        Example Command Scripts: Examples of command scripts for different scenarios, including reading designs, performing timing analysis, and power analysis.
-        Timing Analysis using SDF: Instructions and examples for performing timing analysis using Standard Delay Format (SDF).
-        Timing Analysis with Multiple Process Corners: Guidelines and examples for conducting timing analysis across multiple process corners.
-        Power Analysis: Steps and commands involved in performing power analysis.
-        TCL Interpreter: Details on using the TCL interpreter with OpenSTA.
-        Commands: Comprehensive list and descriptions of all commands available in OpenSTA, along with usage guidelines and examples.
-        Filter Expressions: Information on filter expressions used within OpenSTA commands.
-        Variables: Descriptions of various variables used in OpenSTA and their purposes.
-
+        As a stand-alone executable it can be used to verify the timing of a design using standard file formats.\
+        Verilog netlist\
+        Liberty library\
+        SDC timing constraints\
+        SDF delay annotation\
+        SPEF parasitics\
+        Command Line Arguments: Detailed information about the different command line arguments that can be used with OpenSTA.\
+        Example Command Scripts: Examples of command scripts for different scenarios, including reading designs, performing timing analysis, and power analysis.\
+        Timing Analysis using SDF: Instructions and examples for performing timing analysis using Standard Delay Format (SDF).\
+        Timing Analysis with Multiple Process Corners: Guidelines and examples for conducting timing analysis across multiple process corners.\
+        Power Analysis: Steps and commands involved in performing power analysis.\
+        TCL Interpreter: Details on using the TCL interpreter with OpenSTA.\
+        Commands: Comprehensive list and descriptions of all commands available in OpenSTA, along with usage guidelines and examples.\
+        Filter Expressions: Information on filter expressions used within OpenSTA commands.\
+        Variables: Descriptions of various variables used in OpenSTA and their purposes.\
         """
         if RetrieverAgent.opensta_retriever is None:
             raise ValueError('OpenSTA Retriever not initialized')
@@ -261,9 +253,9 @@ class RetrieverAgent:
     @tool
     def retrieve_errinfo(query: str) -> Tuple[str, list[str], list[str]]:
         """
-        Retrieve descriptions and details regarding the various warning/error messages encountered while using the OpenROAD.
-        An error code usually is identified by the tool, followed by a number.
-        Examples: ANT-0001, CTS-0014 etc.
+        Retrieve descriptions and details regarding the various warning/error messages encountered while using the OpenROAD.\
+        An error code usually is identified by the tool, followed by a number.\
+        Examples: ANT-0001, CTS-0014 etc.\
         """
 
         if RetrieverAgent.errinfo_retriever is None:
@@ -276,12 +268,12 @@ class RetrieverAgent:
     @tool
     def retrieve_yosys_rtdocs(query: str) -> Tuple[str, list[str], list[str]]:
         """
-        Retrieve detailed information regarding the Yosys application. \
+        Retrieve detailed information regarding the Yosys application.\
         This tool provides comprehensive information on the various functionalities, commands, and usage guidelines of Yosys.\
         This tool provides information pertaining to the installation, usage, and troubleshooting of Yosys.\
         
-        Yosys is a framework for Verilog RTL synthesis. \
-        It currently has extensive Verilog-2005 support and provides a basic set of synthesis algorithms for various application domains. \
+        Yosys is a framework for Verilog RTL synthesis.\
+        It currently has extensive Verilog-2005 support and provides a basic set of synthesis algorithms for various application domains.\
         """
 
         if RetrieverAgent.yosys_rtdocs_retriever is None:
@@ -289,3 +281,4 @@ class RetrieverAgent:
 
         docs = RetrieverAgent.yosys_rtdocs_retriever.invoke(input=query)
         return format_docs(docs)
+
