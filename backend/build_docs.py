2c5c37d854e2007ab75aa98086191f27fc4223fc
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-08-31 18:03:30 +0530

    feat: add fn to scrape klayout docs
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/build_docs.py
+++ backend/build_docs.py
@@ -21,6 +21,7 @@ opensta_readme_url = (
     'https://raw.githubusercontent.com/The-OpenROAD-Project/OpenSTA/master/README.md'
 )
 yosys_html_url = 'https://yosyshq.readthedocs.io/projects/yosys/en/latest'
+klayout_html_url = 'https://www.klayout.de/doc.html'
 or_website_url = 'https://theopenroadproject.org/'
 
 logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO').upper())
@@ -41,6 +42,8 @@ def update_src(src_path: str, dst_path: str) -> None:
         )
     elif 'yosys' in dst_path:
         source_dict[dst_path] = f"https://{dst_path[len('data/html/yosys_docs') :]}"
+    elif 'klayout' in dst_path:
+        source_dict[dst_path] = f"https://{dst_path[len('data/html/klayout_docs') :]}"
     elif 'OpenSTA' in dst_path and 'pdf' in dst_path:
         source_dict[dst_path] = opensta_docs_url
     elif 'OpenSTA' in dst_path and 'markdown' in dst_path:
@@ -387,11 +390,26 @@ def get_yosys_docs_html() -> None:
             shell=True,
         )
     except Exception as e:
-        logging.debug(f'Error in downloading Yosys RT docs: {e}')
+        logging.debug(f'Error in downloading Yosys docs: {e}')
         sys.exit(1)
 
-    logging.debug('Yosys RT docs downloaded successfully.')
+    logging.debug('Yosys docs downloaded successfully.')
     track_src(f'{cur_dir}/data/html/yosys_docs')
+
+
+def get_klayout_docs_html() -> None:
+    logging.debug('Scraping KLayout docs...')
+    try:
+        subprocess.run(
+            f'wget -r -A.html -l 3 -P data/html/klayout_docs {klayout_html_url} ',
+            shell=True,
+        )
+    except Exception as e:
+        logging.debug(f'Error in downloading KLayout docs: {e}')
+        sys.exit(1)
+
+    logging.debug('KLayout docs downloaded successfully.')
+    track_src(f'{cur_dir}/data/html/klayout_docs')
 
 
 if __name__ == '__main__':
@@ -419,10 +437,13 @@ if __name__ == '__main__':
     os.makedirs('data/pdf/OR_publications', exist_ok=True)
     os.makedirs('data/html', exist_ok=True)
 
+    get_klayout_docs_html()
+    get_yosys_docs_html()
+    
     get_or_publications()
     get_or_website_html()
     get_opensta_docs()
-    get_yosys_docs_html()
+
 
     clone_repo(
         url='https://github.com/The-OpenROAD-Project/OpenROAD.git',
