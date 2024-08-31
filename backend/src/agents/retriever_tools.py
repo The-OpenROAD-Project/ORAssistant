4b47bc66298de70b216d93f49105d6d3292fe508
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-08-31 18:02:47 +0530

    feat: add klayout tool, move opensta to commands
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/agents/retriever_tools.py
+++ backend/src/agents/retriever_tools.py
@@ -27,9 +27,6 @@ class RetrieverTools:
     general_retriever: Optional[
         Union[EnsembleRetriever, ContextualCompressionRetriever]
     ]
-    opensta_retriever: Optional[
-        Union[EnsembleRetriever, ContextualCompressionRetriever]
-    ]
     commands_retriever: Optional[
         Union[EnsembleRetriever, ContextualCompressionRetriever]
     ]
@@ -37,6 +34,9 @@ class RetrieverTools:
         Union[EnsembleRetriever, ContextualCompressionRetriever]
     ]
     yosys_rtdocs_retriever: Optional[
+        Union[EnsembleRetriever, ContextualCompressionRetriever]
+    ]
+    klayout_retriever: Optional[
         Union[EnsembleRetriever, ContextualCompressionRetriever]
     ]
     tool_descriptions: str = ''
@@ -98,7 +98,9 @@ class RetrieverTools:
                 './data/markdown/gh_discussions/Documentation',
                 './data/markdown/manpages/man1',
                 './data/markdown/manpages/man2',
+                './data/markdown/OpenSTA_docs',
             ],
+            other_docs_path=['./data/pdf'],
             weights=[0.6, 0.2, 0.2],
             contextual_rerank=True,
             search_k=search_k,
@@ -120,19 +122,18 @@ class RetrieverTools:
         yosys_rtdocs_retriever_chain.create_hybrid_retriever()
         RetrieverTools.yosys_rtdocs_retriever = yosys_rtdocs_retriever_chain.retriever
 
-        opensta_retriever_chain = HybridRetrieverChain(
+        klayout_retriever_chain = HybridRetrieverChain(
             embeddings_config=embeddings_config,
             reranking_model_name=reranking_model_name,
             use_cuda=use_cuda,
-            markdown_docs_path=['./data/markdown/OpenSTA_docs'],
-            other_docs_path=['./data/pdf/OpenSTA'],
+            html_docs_path=['./data/html/KLayout_docs'],
             weights=[0.6, 0.2, 0.2],
             contextual_rerank=True,
             search_k=search_k,
             chunk_size=chunk_size,
         )
-        opensta_retriever_chain.create_hybrid_retriever()
-        RetrieverTools.opensta_retriever = opensta_retriever_chain.retriever
+        klayout_retriever_chain.create_hybrid_retriever()
+        RetrieverTools.klayout_retriever = klayout_retriever_chain.retriever
 
         errinfo_retriever_chain = HybridRetrieverChain(
             embeddings_config=embeddings_config,
@@ -169,18 +170,23 @@ class RetrieverTools:
     @tool
     def retrieve_cmds(query: str) -> Tuple[str, list[str], list[str]]:
         """
-        Retrieve information on the commands available in the OpenROAD project and OpenROAD-Flow-Scripts.\
+        Retrieve information on the commands available in the OpenROAD, OpenROAD-Flow-Scripts and OpenSTA.\
         This includes usage guidelines, command syntax, examples, and best practices about commands that cover various \
         aspects of electronic design automation, such as synthesis, placement, routing, analysis, and \
         optimization within the OpenROAD environment.
 
-        Commands include:
+        OR and ORFS Commands:
         Antenna Rule Checker (ANT), Clock Tree Synthesis (CTS), Design For Testing (DFT), Detailed Placement (DPL), \
         Detailed Routing (DRT), Metal Fill (FIN), Floorplanning, Global Placement (GPL), Global Routing (GRT), Graphical User Interface (GUI), \
         Initialize Floorplan (IFP), Macro Placement (MPL), Hierarchical Macro Placement (MPL2), OpenDB (ODB), Chip-level Connections (PAD),\
         Partition Manager (PAR), Power Distribution Network (PDN), Pin Placement (PPL), IR Drop Analysis (PSM), Parasitics Extraction (RSX),\
         Restructure (RMP), Gate Resizer (RSZ), Rectilinear Steiner Tree (STT), TapCell (TAP), Read Unified Power Format (UPF), Timing Optimization\
        
+        OpenSTA is an open-source gate-level static timing verifier that has been used by many design houses.\
+        Timing Analysis: Perform static timing analysis using standard file formats (Verilog, Liberty, SDC, SDF, SPEF).
+        Multiple Process Corners: Conduct analysis across different process variations.
+        Power Analysis: Evaluate power consumption in designs.
+        TCL Interpreter: Use TCL scripts for command automation and customization.
         """
         if RetrieverTools.commands_retriever is None:
             raise ValueError('Commands Retriever not initialized')
@@ -206,27 +212,6 @@ class RetrieverTools:
 
     @staticmethod
     @tool
-    def retrieve_opensta(query: str) -> Tuple[str, list[str], list[str]]:
-        """
-        Retrieve detailed information regarding the OpenSTA application.\
-        This tool provides information pertaining to the installation, usage, and troubleshooting of OpenSTA.\
-        This tool provides comprehensive information on the various functionalities, commands, and usage guidelines of OpenSTA.\
-        
-        OpenSTA is an open-source gate-level static timing verifier that has been used by many design houses.\
-        Timing Analysis: Perform static timing analysis using standard file formats (Verilog, Liberty, SDC, SDF, SPEF).
-        Multiple Process Corners: Conduct analysis across different process variations.
-        Power Analysis: Evaluate power consumption in designs.
-        TCL Interpreter: Use TCL scripts for command automation and customization.
-   
-        """
-        if RetrieverTools.opensta_retriever is None:
-            raise ValueError('OpenSTA Retriever not initialized')
-
-        docs = RetrieverTools.opensta_retriever.invoke(input=query)
-        return format_docs(docs)
-
-    @staticmethod
-    @tool
     def retrieve_errinfo(query: str) -> Tuple[str, list[str], list[str]]:
         """
         Retrieve descriptions and details regarding the various warning/error messages encountered while using the OpenROAD.\
@@ -246,7 +231,6 @@ class RetrieverTools:
         """
         Retrieve detailed information regarding the Yosys application.\
         This tool provides information pertaining to the installation, usage, and troubleshooting of Yosys.\
-        This tool provides comprehensive information on the various functionalities, commands, and usage guidelines of Yosys.\
         
         Yosys is a framework for Verilog RTL synthesis.\
         It currently has extensive Verilog-2005 support and provides a basic set of synthesis algorithms for various application domains.\
@@ -259,4 +243,21 @@ class RetrieverTools:
             raise ValueError('Yosys RTDocs Retriever not initialized')
 
         docs = RetrieverTools.yosys_rtdocs_retriever.invoke(input=query)
+        return format_docs(docs)
+
+    @staticmethod
+    @tool
+    def retrieve_klayout_docs(query: str) -> Tuple[str, list[str], list[str]]:
+        """
+        Retrieve detailed information regarding the KLayout application.\
+        This tool provides information pertaining to the installation, usage, and troubleshooting of KLayout.\
+        
+        KLayout is a powerful open-source layout viewer and editor designed for integrated circuit (IC) design.\
+        It supports various file formats, including GDSII, OASIS, and DXF
+        """
+
+        if RetrieverTools.klayout_retriever is None:
+            raise ValueError('KLayout Retriever not initialized')
+
+        docs = RetrieverTools.klayout_retriever.invoke(input=query)
         return format_docs(docs)
