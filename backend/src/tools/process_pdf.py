79bab0894c78ed8c39ea829029422132498742da
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-09-13 10:52:06 +0530

    feat: log corrupted pdf error
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/src/tools/process_pdf.py
+++ backend/src/tools/process_pdf.py
@@ -2,6 +2,7 @@ import os
 import json
 import logging
 from dotenv import load_dotenv
+from pypdf.errors import PdfStreamError
 
 from langchain.docstore.document import Document
 from langchain_community.document_loaders import PyPDFLoader
@@ -26,14 +27,17 @@ def process_pdf_docs(file_path: str) -> list[Document]:
     with open('data/source_list.json') as f:
         src_dict = json.loads(f.read())
 
-    documents = loader.load_and_split(text_splitter=text_splitter)
-
+    try:
+        documents = loader.load_and_split(text_splitter=text_splitter)
+    except PdfStreamError:
+        logging.error(f"Error processing PDF: {file_path} is corrupted or incomplete.")
+    
     for doc in documents:
         try:
             doc.metadata['source'] = file_path.split('./')[-1]
             url = src_dict[doc.metadata['source']]
         except KeyError:
-            logging.warn(f"Could not find source for {doc.metadata['source']}")
+            logging.error(f"Could not find source for {doc.metadata['source']}")
             url = ''
 
         new_metadata = {
