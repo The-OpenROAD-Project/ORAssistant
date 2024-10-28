cdc4df34420fd2d67177245f43093a1ff283594b
Palaniappan R <palaniappan.r.mail@gmail.com>
2024-10-29 01:05:27 +0530

    fix: update import statement in main.py
    Signed-off-by: Palaniappan R <palaniappan.r.mail@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,17 @@ import os
 import uvicorn
 from dotenv import load_dotenv
 
-from backend.src.api.main import app
+from src.api.main import app
 
 load_dotenv()
 
 
 def main() -> None:
     uvicorn.run(
-        app, host=os.getenv('BACKEND_URL','0.0.0.0'), port=8000, workers=int(os.getenv('BACKEND_WORKERS', 1))
+        app,
+        host=os.getenv("BACKEND_URL", "0.0.0.0"),
+        port=8000,
+        workers=int(os.getenv("BACKEND_WORKERS", 1)),
     )
 
 
