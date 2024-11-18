49533246694656171a82ffad4cca93a22a7191aa
Kannav02 <kannavsethi02@gmail.com>
2024-11-18 17:52:17 -0500

    changes made for my refernce
    Signed-off-by: Kannav02 <kannavsethi02@gmail.com>

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-
-from .routers import graphs, healthcheck
+from .routers import graphs, healthcheck,chains
 
 app = FastAPI()
 
 app.include_router(healthcheck.router)
 app.include_router(graphs.router)
+app.include_router(chains.router)
