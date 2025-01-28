75f01632ecd45bcf52dff5279aaa61312e09495f
Kannav02 <kannavsethi02@gmail.com>
2025-01-27 23:19:14 -0500

    feat: added ContextSource class
    Signed-off-by: Kannav02 <kannavsethi02@gmail.com>

--- backend/src/api/models/response_model.py
+++ backend/src/api/models/response_model.py
@@ -7,11 +7,12 @@ class UserInput(BaseModel):
     list_sources: bool = False
     list_context: bool = False
 
-
+class ContextSource(BaseModel):
+    sources: str = ""
+    context: str = ""
 class ChatResponse(BaseModel):
     response: str
-    sources: list[str] = []
-    context: list[str] = []
+    context_sources: list[ContextSource] = []
     tools: list[str] = []
 
 
