653c3b7277a6c1ddd87d37f852628b9345b84f5e
Kannav02 <kannavsethi02@gmail.com>
2025-04-07 21:08:15 -0400

    feat: added the helper path for the routers
    Signed-off-by: Kannav02 <kannavsethi02@gmail.com>

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from .routers import graphs, healthcheck
+from .routers import graphs, healthcheck, helpers
 
 app = FastAPI()
 
 app.include_router(healthcheck.router)
 app.include_router(graphs.router)
+app.include_router(helpers.router)
