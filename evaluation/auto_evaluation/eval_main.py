10eea095412457f2763cc5c643b896b2ff6285d0
Jack Luar <jluar@precisioninno.com>
2025-06-03 19:26:41 +0000

    revert debug
    Signed-off-by: Jack Luar <jluar@precisioninno.com>

--- evaluation/auto_evaluation/eval_main.py
+++ evaluation/auto_evaluation/eval_main.py
@@ -77,9 +77,6 @@ class EvaluationHarness:
 
         # retrieval test cases
         for i, qa_pair in enumerate(tqdm(self.qns, desc="Evaluating")):
-            if i >= 1:
-                print("Skipping further evaluations for testing purposes.")
-                break
             question, ground_truth = qa_pair["question"], qa_pair["ground_truth"]
             response, response_time = self.query(retriever, question)
             response_text = response["response"]
