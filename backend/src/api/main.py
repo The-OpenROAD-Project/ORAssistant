1cac0ece1c458252c11f54bfad34fe57ab1f7823
Kannav02 <kannavsethi02@gmail.com>
2025-06-04 22:29:00 -0400

    feat: added middleware and new router for ui
    Signed-off-by: Kannav02 <kannavsethi02@gmail.com>

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
-from .routers import graphs, healthcheck, helpers
+from .routers import graphs, healthcheck, helpers, ui
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:8001",  # mock endpoint
+        "http://127.0.0.1:8001",
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 app.include_router(healthcheck.router)
 app.include_router(graphs.router)
 app.include_router(helpers.router)
+app.include_router(ui.router)
