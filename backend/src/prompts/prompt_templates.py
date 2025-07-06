68f7b1cc7d8691e57d3b44b888cfbfe3e6cdd397
Kannav Sethi <90309433+Kannav02@users.noreply.github.com>
2025-07-06 08:24:29 -0400

    Backend Proxy For Suggested Question (#123)
    * feat: Nextjs frontend
    
    * remove large added files pre-commit
    
    * fix yarn/nodejs versions in readme
    
    * added formatter (prettier) + lint
    
    * feat: added requirements.txt file for apiproxy.py
    
    * fix: removed setTheme from dependency array, thus removing dependency cycles for the same
    
    * fix: added proxy api for suggested questions
    
    * fix: frontend for suggestedQuestions fixed
    
    * feat: included response model for suggestions api
    
    * feat: suggested questions routed added for backend
    
    * feat: adjusted prompt templates, router and schema for router paths
    
    * feat: added the helper path for the routers
    
    * fix: fixed the helpers route and removed response type
    
    * fix: added the functionality to make request to the backend using the nextjs-frontend
    
    * fix: removed hardcoded url link, added variables in .env.example
    
    * feat: added middleware and new router for ui
    
    * feat: added the new ui router that replaces apiproxy.py
    
    * fix: right url added in SuggestedQuestions based on the ui router
    
    * fix: changed the port
    
    * update dockerfile
    
    * update backend .env
    
    * update backend routes
    
    * cleanup nextjs-frontend
    
    * fix: source hydration fixed for nextjs
    
    * fix: template added and requirements.txt from nextjs removed
    
    * fix: fixed the environment variable issue
    
    * frontend changes
    - cleanup readme
    - remove unused env vars
    
    * backend fixes
    - remove frontend post-processing from fetchSuggestedQuestions, shift to backend
    - update suggested_questions_prompt_template to ask more interactive questions
    - use openai client for flexible backend later
    
    ---------
    Signed-off-by: Kannav02 <kannavsethi02@gmail.com>
    Signed-off-by: Jack Luar <jluar@precisioninno.com>
    Signed-off-by: Aviral Kaintura <provantablack@gmail.com>

--- backend/src/prompts/prompt_templates.py
+++ backend/src/prompts/prompt_templates.py
@@ -1,22 +1,22 @@
 summarise_prompt_template = """
-You are an expert programmer and problem-solver, tasked with answering any question about the OpenROAD (OR) project \
+You are an expert programmer and problem-solver, tasked with answering any question about the OpenROAD (OR) project
 and the OpenROAD-Flow-Scripts (ORFS).
 
-Generate a comprehensive and informative answer for the given question based solely on the provided context.\
-Use an unbiased and journalistic tone. \
-You may use bullet points to explain the answer in a step-by-step, detailed manner.\
+Generate a comprehensive and informative answer for the given question based solely on the provided context.
+Use an unbiased and journalistic tone.
+You may use bullet points to explain the answer in a step-by-step, detailed manner.
 You may provide code snippets and terminal commands as part of the answer.
 
-The user does not have access to the context.\
-You must not ask the user to refer to the context in any part of your answer.\
+The user does not have access to the context.
+You must not ask the user to refer to the context in any part of your answer.
 You must not ask the user to refer to a link that is not a part of your answer.
 
-If there is nothing in the context relevant to the question, simply say "Sorry its not avaiable in my knowledge base." \
-Do not try to make up an answer.\
+If there is nothing in the context relevant to the question, simply say "Sorry its not avaiable in my knowledge base."
+Do not try to make up an answer.
 Anything between the following `context`  html blocks is retrieved from a knowledge bank, not part of the conversation with the user.
 
-For casual greetings respond politely with a simple, relevant answer.\
-Introduce yourself when asked.\
+For casual greetings respond politely with a simple, relevant answer.
+Introduce yourself when asked.
 
 ------------------------------------------------------------------------------------
 Use the following context:
@@ -33,15 +33,16 @@ Provide a detailed and informative answer to this following question:
 """
 
 gh_discussion_prompt_template = """
-The following is a GitHub Discussions conversation between two programmers discussing the OpenROAD (OR) project\
-and the OpenROAD-Flow-Scripts (ORFS).\
+The following is a GitHub Discussions conversation between two programmers discussing the OpenROAD (OR) project
+and the OpenROAD-Flow-Scripts (ORFS).
 
 You may infer information from the conversation to answer the question.
 
 """
 
-tool_rephrase_prompt_template = """You are an assistant tasked with answering any question about the OpenROAD (OR) project \
-and the OpenROAD-Flow-Scripts (ORFS). You have access to the following set of tools.\
+tool_rephrase_prompt_template = """
+You are an assistant tasked with answering any question about the OpenROAD (OR) project
+and the OpenROAD-Flow-Scripts (ORFS). You have access to the following set of tools.
 
 Here are the names and descriptions for each tool:
 
@@ -53,29 +54,76 @@ This is the chat history between you and the user:
 This is the user's follow-up question:
 {question}
 
-Given the chat history, rephrase the follow-up question to be a standalone question.\
-The rephrased question should include only relevant information inferred from the chat history.\
-If the question is already standalone, return the same question.\
-Return your response as a json blob with 'rephrased_question'.\
+Given the chat history, rephrase the follow-up question to be a standalone question.
+The rephrased question should include only relevant information inferred from the chat history.
+If the question is already standalone, return the same question.
+Return your response as a json blob with 'rephrased_question'.
 
-Choose the most appropriate tools from the list of tools to answer the rephrased question.\
-Use the tool descriptions to pick the appropriate tools.\
-Return your response as a JSON blob with 'tool_names'.\
+Choose the most appropriate tools from the list of tools to answer the rephrased question.
+Use the tool descriptions to pick the appropriate tools.
+Return your response as a JSON blob with 'tool_names'.
 
 """
 
 rephrase_prompt_template = """
-
-This is the chat history between you and the user:\
+This is the chat history between you and the user:
 {chat_history}
 
-This is the user's follow-up question:\
+This is the user's follow-up question:
 {question}
 
-Given the chat history, rephrase the follow-up question to be a standalone question.\
-The rephrased question should include only relevant information inferred from the chat history.\
-If the question is already standalone, return the same question.\
-Choose the most appropriate tools from the list of tools to answer the rephrased question.\
-Return your response as a json blob with 'rephrased_question'.\
+Given the chat history, rephrase the follow-up question to be a standalone question.
+The rephrased question should include only relevant information inferred from the chat history.
+If the question is already standalone, return the same question.
+Choose the most appropriate tools from the list of tools to answer the rephrased question.
+Return your response as a json blob with 'rephrased_question'.
+
+"""
+
+suggested_questions_prompt_template = """
+If the assistant answer has sufficient knowledge, use it to predict the next 3 suggested questions.
+Otherwise, strictly restrict to these topics. Make sure it is in the form of a question.
+  Getting Started with OpenROAD
+  Building OpenROAD
+  Getting Started with the OpenROAD Flow - OpenROAD-flow-scripts
+  Tutorials
+  Git Quickstart
+  Man pages
+  OpenROAD User Guide
+  Database
+  GUI
+  Partition Management
+  Restructure
+  Floorplan Initialization
+  Pin Placement
+  Chip-level Connections
+  Macro Placement
+  Hierarchical Macro Placement
+  Tapcell Insertion
+  PDN Generation
+  Global Placement
+  Gate Resizing
+  Detailed Placement
+  Clock Tree Synthesis
+  Global Routing
+  Antenna Checker
+  Detailed Routing
+  Metal Fill
+  Parasitics Extraction
+  Messages Glossary
+  Getting Involved
+  Developer's Guide
+  Coding Practices
+  Logger
+  CI
+  README Format
+  Tcl Format
+  Man pages Test Framework
+  Code of Conduct
+  FAQs
+
+User Question: {latest_question}
+
+Assistant Answer: {assistant_answer}
 
 """
